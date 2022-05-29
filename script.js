c5366f5b8924375bce6b239a2fce83e5ee824e3e
Пользователь <ina_gencer@mail.ru>
2022-05-30 01:29:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+gsap.from('.padding', {opacity: 0, duration: .7, stagger: .4})  
+gsap.from('.cupofcoffe', {opacity: 0, duration: 2, delay: 1})
+gsap.from('.about', {opacity: 0, duration: 2, delay: 2})
+gsap.from('.part', { opacity: 0, duration: 2.5, delay: 2.3, stagger: .6})
+gsap.from('.head', {ease: "bounce", y: 10, duration: 3, repeat: -1})
+
